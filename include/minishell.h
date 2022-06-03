bfa297c50ccd11517b6495fd95ff18007185603e
fenix144 <yan_1731@hotmail.com>
2022-06-02 22:18:12 -0400

    Forgot to add folders

---
+++ include/minishell.h
@@ -0,0 +1,30 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <signal.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
+
+#endif
+
+// readline, rl_clear_history, rl_on_new_line,
+// rl_replace_line, rl_redisplay, add_history,
+// printf, malloc, free, write, access, open, read,
+// close, fork, wait, waitpid, wait3, wait4, signal,
+// sigaction, sigemptyset, sigaddset, kill, exit,
+// getcwd, chdir, stat, lstat, fstat, unlink, execve,
+// dup, dup2, pipe, opendir, readdir, closedir,
+// strerror, perror, isatty, ttyname, ttyslot, ioctl,
+// getenv, tcsetattr, tcgetattr, tgetent, tgetflag,
+// tgetnum, tgetstr, tgoto, tputs
